e0d0ea5b4a34ba4c4b3d0abb348a690d281aa57d
kevinpilarczyk <kpilarczyk@paretointel.com>
2023-01-09 15:57:57 -0600

    Add s3 write

--- writes3.py
+++ writes3.py
@@ -4,8 +4,8 @@ unload_query = """
             iam_role '{iam_role}'
             {unload_options};
             """.format(select_query=select_query,
-                       s3_unload_path=s3_unload_path,
-                       iam_role=iam_role,
-                       unload_options=unload_options)
-hook.run(unload_query, autocommit)
-log.info("UNLOAD command complete...")
+                       s3_unload_path=self.s3_unload_path,
+                       iam_role=self.iam_role,
+                       unload_options=self.unload_options)
+self.hook.run(unload_query, self.autocommit)
+self.log.info("UNLOAD command complete...")
